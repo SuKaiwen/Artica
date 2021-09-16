f7042d8c3cb2ee076fe67d3ef19d9ad3507a1981
Kevin Su <kevinsu@outlook.co.nz>
2021-09-16 17:40:17 +1200

    cart functionality finished

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -4,21 +4,53 @@ function Cart({cart, setCart}) {
     setCart([]);
   }
 
+  const appendItem = (item) => {
+      console.log(cart);
+      var temp = [...cart];
+      if(cart.filter(e => e.id === item.id).length > 0){
+          let updated = temp.find(({id}) => id === item.id);
+          updated.quantity = updated.quantity + 1;
+      }else{
+          temp.push(item);
+      }
+      setCart(temp);
+    }
+
+  const removeItem = (item) => {
+        console.log(cart);
+        var temp = [...cart];
+        if(cart.filter(e => e.id === item.id).length > 0){
+            let updated = temp.find(({id}) => id === item.id);
+            updated.quantity = updated.quantity - 1;
+            if(updated.quantity < 1){
+                var x = cart.filter(x => x.id !== updated.id);
+                setCart(x);
+                return;
+            }
+        }else{
+            return;
+        }
+        setCart(temp);
+      }
+
   return (
     <div className="home">
       <div className="title">My Cart</div>
       <div className="row">
         {cart.length < 1 ? ( <div className="col-sm-4">Cart is currently empty</div>) :
-            <div className="col-sm-5">
+            <div className="col-sm-6" style={{maxWidth:"300px"}}>
               {cart.map((x) => {
                 return(
-                    <div>
+                    <div style={{textAlign:"center",  marginTop:"30px"}}>
+                        <hr></hr>
                         <img src={x.src} className="roundedImage" alt="img" />
+                        <p>{x.name}</p>
+                        <p>${x.price}</p>
                         <div style={{display:"flex", justifyContent:"space-between"}}>
-                            <p>{x.name}</p>
-                            <p>${x.price}</p>
+                            <button onClick={()=>removeItem(x)}className="btn btn-danger">-</button>
+                            <p>Quantity: {x.quantity} </p>
+                            <button onClick={()=>appendItem(x)}className="btn btn-success">+</button>
                         </div>
-                        <p>Quantity: {x.quantity}</p>
                     </div>
                 )
               })}
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,29 @@
 import {Link} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
 
 function Header({cart}) {
+
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    getTotal();
+  }, [cart])
+
+  const getTotal = () => {
+    let x = 0;
+    for (var i = 0; i < cart.length; i++){
+        x = x + cart[i].quantity;
+    }
+    console.log(x);
+    setTotal(x);
+  }
+
   return (
     <div className="header">
       <Link className="link" to="/"><h5>Artica</h5></Link>
       <div style={{justifyContent:"space-between", width:"230px", display:'flex'}}>
           <Link className="link" to="/products"><p>Products</p></Link>
-          <Link className="link" to="/cart"><p>Cart <span className="badge badge-primary">{`${cart.length}`}</span></p></Link>
+          <Link className="link" to="/cart"><p>Cart <span className="badge badge-primary">{`${total}`}</span></p></Link>
           <Link className="link" to="/aboutus"><p>About Us</p></Link>
       </div>
     </div>
